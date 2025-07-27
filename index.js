820dbd68e51e165ceeb0f662b39820e4266761c6
nithish reddy <nithishreddy20031007@gmail.com>
2025-07-27 21:55:45 +0530

    added some more quotes

--- index.js
+++ index.js
@@ -62,22 +62,36 @@ const quotes = {
     inspirational: [
         { quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.", author: "Nelson Mandela" },
         { quote: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
-        { quote: "It is during our darkest moments that we must focus to see the light.", author: "Aristotle" }
+        { quote: "It is during our darkest moments that we must focus to see the light.", author: "Aristotle" },
+        { quote: "Get busy living or get busy dying.", author: "Stephen King" },
+        { quote: "You have within you right now, everything you need to deal with whatever the world can throw at you.", author: "Brian Tracy" },
+        { quote: "The only limit to our realization of tomorrow will be our doubts of today.", author: "Franklin D. Roosevelt" }
     ],
     success: [
         { quote: "The way to get started is to quit talking and begin doing.", author: "Walt Disney" },
         { quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.", author: "Winston Churchill" },
-        { quote: "I find that the harder I work, the more luck I seem to have.", author: "Thomas Jefferson" }
+        { quote: "I find that the harder I work, the more luck I seem to have.", author: "Thomas Jefferson" },
+        { quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" },
+        { quote: "The only impossible journey is the one you never begin.", author: "Tony Robbins" },
+        { quote: "Success usually comes to those who are too busy to be looking for it.", author: "Henry David Thoreau" }
     ],
     life: [
         { quote: "Your time is limited, so don't waste it living someone else's life.", author: "Steve Jobs" },
         { quote: "If life were predictable it would cease to be life, and be without flavor.", author: "Eleanor Roosevelt" },
-        { quote: "The purpose of our lives is to be happy.", author: "Dalai Lama" }
+        { quote: "The purpose of our lives is to be happy.", author: "Dalai Lama" },
+        { quote: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+        { quote: "Act as if what you do makes a difference. It does.", author: "William James" },
+        { quote: "In the end, it's not the years in your life that count. It's the life in your years.", author: "Abraham Lincoln" },
+        { quote: "Life is either a daring adventure or nothing at all.", author: "Helen Keller" }
+
     ],
     humor: [
         { quote: "I'm sorry, if you were right, I'd agree with you.", author: "Robin Williams" },
         { quote: "I always wanted to be somebody, but now I realize I should have been more specific.", author: "Lily Tomlin" },
-        { quote: "If you think you are too small to make a difference, try sleeping with a mosquito.", author: "Dalai Lama" }
+        { quote: "If you think you are too small to make a difference, try sleeping with a mosquito.", author: "Dalai Lama" },
+        { quote: "I used to think I was indecisive, but now I'm not too sure.", author: "Unknown" },
+        { quote: "I told my wife the truth. I told her I was seeing a psychiatrist. Then she told me the truth: that she was seeing a psychiatrist, two plumbers, and a bartender.", author: "Rodney Dangerfield" },
+    
     ]
 };
 
